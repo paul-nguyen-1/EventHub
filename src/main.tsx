9908aed3ca920eb73fb7c1888c12f92495594f8a
paul-nguyen96 <paul121596@icloud.com>
2024-11-15 01:43:57 -0600

    tailwind config and set up nav

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./components/router/routes";
+import "./styles/global.css";
+import "normalize.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
