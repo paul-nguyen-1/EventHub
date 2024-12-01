7ec2eab8445c1a8bad9bdd53320aa7ee55ee9368
paul-nguyen96 <paul121596@icloud.com>
2024-11-30 23:31:13 -0600

    carousel functionality done -- work on styling later

--- src/components/common/carousel/cardsCarousel.tsx
+++ src/components/common/carousel/cardsCarousel.tsx
@@ -4,13 +4,17 @@ import "../../../styles/global.css";
 
 interface cardsCarouselProps {
   apiData: CardProps[] | null;
+  reverse?: boolean;
 }
 
 export function CardsCarousel(props: cardsCarouselProps) {
-  const { apiData } = props;
+  const { apiData, reverse } = props;
+
   const containerAnimation = {
     animate: {
-      y: [0, -1300, 0],
+      y: reverse
+        ? [-1300, 0, -1300] // Reverse: Start at the bottom, move to the top, and return back to bottom
+        : [0, -1300, 0], // Default: Start at the top, move to the bottom, and return back to top
     },
     transition: {
       duration: 80,
@@ -20,18 +24,13 @@ export function CardsCarousel(props: cardsCarouselProps) {
   };
 
   const slides = apiData?.map((item) => (
-    <div key={item.id}>
+    <div key={item.id} className="mb-[25px]">
       <Card {...item} />
     </div>
   ));
 
   return (
-    <div
-      className="verticalCarousel"
-      style={{
-        overflow: "hidden",
-      }}
-    >
+    <div className="verticalCarousel">
       <motion.div
         animate={containerAnimation.animate}
         transition={containerAnimation.transition}
--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,4 +1,4 @@
-import { Container } from "@mantine/core";
+import { Container, Flex, Text } from "@mantine/core";
 import { CardsCarousel } from "./common/carousel/cardsCarousel";
 import { useFetchData } from "./common/util";
 import { eventBase } from "./const/apiPaths";
@@ -10,8 +10,19 @@ export const Hero = () => {
 
   return (
     <>
-      <Container>
-        <CardsCarousel apiData={data?._embedded?.events || null} />
+      <Container className="flex min-w-full">
+        <div className="flex justify-center flex-col justify-center">
+          <Text>Discover the best</Text>
+          <Text>Live Events</Text>
+          <Text>
+            Use it to create cards, dropdowns, modals and other components that
+            require background with shadow
+          </Text>
+        </div>
+        <Flex direction="row">
+          <CardsCarousel apiData={data?._embedded?.events} />
+          <CardsCarousel apiData={data?._embedded?.events} reverse={true} />
+        </Flex>
       </Container>
     </>
   );
